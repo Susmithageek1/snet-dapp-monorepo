81ef2c62f3eba30ac0625ca15fda9442db4d7d26
Susmithashanmugam <87366584+Susmithageek1@users.noreply.github.com>
2021-09-17 12:03:46 +0530

    Update index.js

--- packages/stake-dapp/src/Pages/Signup/index.js
+++ packages/stake-dapp/src/Pages/Signup/index.js
@@ -16,11 +16,8 @@ const Signup = props => {
       await dispatch(signupActions.signup(nickname, email, password));
       history.push(GlobalRoutes.SIGNUP_CONFIRM.path);
     } catch (error) {
-      if (error.name === "AuthError") {
-        setSignupError(error.message);
-        return;
-      }
-      setSignupError("Singup Failed");
+      
+      setSignupError(error.message);
     }
   };
   return <SNETSignup info={signupInfo} onSubmit={handleSubmit} signupError={signupError} />;
